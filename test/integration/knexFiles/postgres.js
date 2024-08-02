fc79aff037214d727317fc4e98a134775a681ddd
Udantha Pathirana <letstalk@udantha.com>
2024-08-02 12:10:15 +0200

    connection update

--- test/integration/knexFiles/postgres.js
+++ test/integration/knexFiles/postgres.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
-
 const knexDataApiClient = require('../../../index');
 
-module.exports = {
+const config = {
   client: knexDataApiClient.postgres,
   connection: {
     secretArn: process.env.DB_SECRET_ARN,
     resourceArn: process.env.DB_RESOURCE_ARN,
     database: process.env.DB_NAME,
-    region: process.env.DB_REGION,
-    options: {
-      endpoint: `http:/0.0.0.0:${  process.env.DATA_API_PORT}`
-    }
+    region: process.env.DB_REGION
   }
 };
+if (process.env.ENDPOINT_URL) {
+  config.options = {
+    endpoint: `${process.env.ENDPOINT_URL}:${process.env.DATA_API_PORT}`
+  };
+}
+module.exports = config;
